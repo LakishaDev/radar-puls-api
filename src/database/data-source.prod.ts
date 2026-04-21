f64a2160fb471feb134f91c285f01ee2ba397d52
Lazar Cvetanovic <lazar.cve@gmail.com>
2026-04-21 18:35:42 +0200

    feat: update database module to include mobile users, referrals, and rewards entities

--- src/database/data-source.prod.ts
+++ src/database/data-source.prod.ts
@@ -8,6 +8,11 @@ import { RawEventEntity } from "./raw-event.entity";
 import { ParsedEventEntity } from "./parsed-event.entity";
 import { MapPushSubscriptionEntity } from "./map-push-subscription.entity";
 import { MobilePushTokenEntity } from "./mobile-push-token.entity";
+import { MobileUserEntity } from "../mobile-users/mobile-user.entity";
+import { ReferralCodeEntity } from "../referrals/referral-code.entity";
+import { ReferralEntity } from "../referrals/referral.entity";
+import { RewardTierEntity } from "../rewards/reward-tier.entity";
+import { RewardClaimEntity } from "../rewards/reward-claim.entity";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -21,6 +26,11 @@ export const AppDataSource = new DataSource({
     EnrichmentCacheEntity,
     AdminActivityLogEntity,
     LocationAliasEntity,
+    MobileUserEntity,
+    ReferralCodeEntity,
+    ReferralEntity,
+    RewardTierEntity,
+    RewardClaimEntity,
   ],
   migrations: ["dist/database/migrations/*.js"],
   synchronize: false,
